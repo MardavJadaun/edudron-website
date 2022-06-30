5a1e3275bc1747f1d0231446863edc3e24c2966a
Mardav Jadaun <mardavjadaun113@gmail.com>
2022-06-30 18:33:05 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './PageCss/App.css';
 import './PageCss/navbar.css';
 import './PageCss/Home.css';
+import './PageCss/Login.css';
 import './PageCss/footer.css';
 import Navbar from './components/Navbar';
 import Home from './Home';
 import Footer from './components/Footer';
+
 // import Title from './Title'
 // import TextForm from './components/TextForm';
 
 
 function App() {
   return(
-    
-    
-     <> 
+    <> 
  
     <Navbar/>
     {/* <Title/> */}
     <Home/>
-
+    <RegistrationForm/>
     <Footer/>
     
     </>
